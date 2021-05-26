c6800822356e3478da6cb57b2f85a9d2712aa255
darpa <46771690+DARPADOS@users.noreply.github.com>
2021-05-25 22:49:47 -0500

    new spring project for modern education

---
+++ src/main/java/pe/edu/upc/moderneducation/ModernEducationApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.moderneducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/moderneducation/ModernEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.moderneducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
